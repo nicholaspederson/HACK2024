73c99e11b0b1efdfeb1a1c5f41a212babba97e07
Aidan St. George <aidanstgeorge1@gmail.com>
2024-10-26 16:46:48 -0500

    finalize file uploads

--- api.py
+++ api.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
-from io import BytesIO
+import json
+import os
+from tempfile import SpooledTemporaryFile
 from typing import List
 
 from DocumentValidation.StudentData import StudentData
 
 from GoogleDrive import quickstart as google_drive
-from DocumentValidation import EvaluateTranscript
+
+# from DocumentValidation import EvaluateTranscript
 
 app = FastAPI()
 
@@ -19,23 +22,34 @@ app.add_middleware(
 )
 
 
+class TempFile(SpooledTemporaryFile):
+    def __init__(self, name=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._name = name  # Private attribute to hold the file name
+
+    @property
+    def name(self):
+        return self._name
+
+
 @app.options("/{any:path}")
 async def options(_: str):
     return {}
 
 
-@app.post("/test")
-async def test_files(file: UploadFile):
-    print(file.filename)
-
-
 @app.post("/")
 async def post_student(files: List[UploadFile] = File(...), request: str = Form(...)):
     local_files = []
-    for file in files:
-        content = await file.read()
-        local_files += BytesIO(content)
+    for i, file in enumerate(files):
+        with open(file.filename or f"upload_file_{i}.pdf", "wb") as tempfile:
+            tempfile.write(await file.read())
+            tempfile.seek(0)
+            local_files.append(tempfile)
 
-    google_drive.main(request, local_files, "")
+    student = StudentData.parse_obj(json.loads(request))
+    google_drive.main(student, local_files, "")
+
+    for local_file in local_files:
+        os.remove(local_file.name)
 
     return "student data recieved"
