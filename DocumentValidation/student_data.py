2b9b18f6420b1ab6fabecc49874f7ed5494e6c7f
David Schieffer <Gdaveonline@gmail.com>
2024-10-26 13:27:57 -0500

    fix adding files

--- DocumentValidation/student_data.py
+++ DocumentValidation/student_data.py
@@ -4,14 +4,14 @@ class StudentData(BaseModel):
     """
     Parse JSON into a class
     """
-    first_name: str
-    mid_name: str
-    last_name: str
-    gender: str
-    dob: str
-    country: str
-    degree_program: str
-    level: str
+    first_name: str = "Alice"
+    mid_name: str = "Bill"
+    last_name: str = "Joe"
+    gender: str = "M"
+    dob: str = "2024-10-26"
+    country: str = "USA"
+    degree_program: str = "BA"
+    level: str = "Good"
 
 # Parse JSON and create an instance
 json_data = '{"first_name": "Alice", "mid_name": "Bill", "last_name": "Joe", "gender": "M", "dob": "2024-10-26", "country": "USA", "degree_program": "BA", "level": "Good"}'
