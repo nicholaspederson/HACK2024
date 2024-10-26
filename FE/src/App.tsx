1d47ffa6ae648bc3ebe88c7b6d8caf999ff38aab
Simon Anderson <simondanderson47@gmail.com>
2024-10-25 21:14:42 -0500

    initial input form

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,6 +1,11 @@
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import './App.css'
+import { Col, Dropdown, Row } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
+
+
 
 function App() {
 
@@ -14,7 +19,42 @@ function App() {
         Tab content for Home
       </Tab>
       <Tab eventKey="student" title="Student">
-        Tab content for Student File Uploads
+
+        <Form>
+          <Row className="mb-3">
+            <Col className="mb-3">
+            <Form.Group className="mb-3" controlId="firstNameCon">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="firstName" placeholder="First Name" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="middleNameCon">
+                <Form.Label>Middle Name</Form.Label>
+                <Form.Control type="middleName" placeholder="Middle Name" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="lastNameCon">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="lastName" placeholder="Last Name" />
+              </Form.Group>
+            </Col>
+              
+            </Row>
+              <Form.Group>
+                <Form.Label>Gender</Form.Label>
+                  <Form.Select aria-label="gender">
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </Form.Select>
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>Date of Birth</Form.Label>
+                <input aria-label="Date" type="date" /> 
+              </Form.Group>
+
+
+        </Form>
+
+
       </Tab>
       <Tab eventKey="validation" title="Validation">
         Tab content for Validation Flow
