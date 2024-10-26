e40fc5d38ce8688070b5a0250386c401c16ffdc9
elementboron <simondanderson47@gmail.com>
2024-10-25 22:07:31 -0500

    fixed spelling/formatting

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -27,61 +27,61 @@ function App() {
 
         <Form>
           <Row className="pt-3">
-          <Col className="mr-3">
-            <Form.Group className="mb-3" controlId="firstNameCon">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="firstName" placeholder="First Name" />
-              </Form.Group>
-          </Col>
-          <Col className="mr-3">
-            <Form.Group className="mb-3" controlId="middleNameCon">
-                <Form.Label>Middle Name</Form.Label>
-                <Form.Control type="middleName" placeholder="Middle Name" />
-              </Form.Group>
-          </Col>
-          <Col className="mr-3">
-              <Form.Group className="mb-3" controlId="lastNameCon">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="lastName" placeholder="Last Name" />
-              </Form.Group>
-          </Col>
+            <Col className="mr-3">
+              <Form.Group className="mb-3" controlId="firstNameCon">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control type="firstName" placeholder="First Name" />
+                </Form.Group>
+            </Col>
+            <Col className="mr-3">
+              <Form.Group className="mb-3" controlId="middleNameCon">
+                  <Form.Label>Middle Name</Form.Label>
+                  <Form.Control type="middleName" placeholder="Middle Name" />
+                </Form.Group>
+            </Col>
+            <Col className="mr-3">
+                <Form.Group className="mb-3" controlId="lastNameCon">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control type="lastName" placeholder="Last Name" />
+                </Form.Group>
+            </Col>
           </Row>
           <Row className="pt-3">
-          <Col className="mr-3">
-            <Form.Group>
-                <Form.Label>Gender</Form.Label>
-                  <Form.Select aria-label="gender">
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                  </Form.Select>
-              </Form.Group>
-          </Col>
-          <Col className="mr-3">
+            <Col className="mr-3">
               <Form.Group>
-                <Form.Label>Date of Birth: </Form.Label>
-                <Form.Control aria-label="date" type="Date" /> 
-              </Form.Group>
-          </Col>
-          <Row className="pt-5">
-          <Col className="mb-3">
-                <Form.Group>
-                  <Form.Label>County of Education for Prerequisite Degrees</Form.Label>
-                  <Form.Control type="country" placeholder = "Country" />
+                  <Form.Label>Gender</Form.Label>
+                    <Form.Select aria-label="gender">
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                    </Form.Select>
                 </Form.Group>
-          </Col>
-          <Col className="mr-3">
-              <Form.Group>
-              <Form.Label>Degree Type</Form.Label>
-                <Form.Select aria-label="degreeLevel">
-                  <option value="undergraduate">Undergraduate</option>
-                  <option value="graduateCertificate">Graduate Certificate</option>
-                  <option value="masters">Masters</option>
-                  <option value="doctoral">Doctoral</option>
-                </Form.Select>
-              </Form.Group>
-          </Col>
+            </Col>
+            <Col className="mr-3">
+                <Form.Group>
+                  <Form.Label>Date of Birth: </Form.Label>
+                  <Form.Control aria-label="date" type="Date" /> 
+                </Form.Group>
+            </Col>
           </Row>
-          
+          <Row className="pt-5">
+            <Col className="mb-3">
+                  <Form.Group>
+                    <Form.Label>Country of Education</Form.Label>
+                    <Form.Control type="country" placeholder = "Country" />
+                  </Form.Group>
+            </Col>
+            <Col className="mr-3">
+                <Form.Group>
+                <Form.Label>Degree Type</Form.Label>
+                  <Form.Select aria-label="degreeLevel">
+                    <option value="undergraduate">Undergraduate</option>
+                    <option value="graduateCertificate">Graduate Certificate</option>
+                    <option value="masters">Masters</option>
+                    <option value="doctoral">Doctoral</option>
+                  </Form.Select>
+                </Form.Group>
+            </Col>
+
           
   
 
