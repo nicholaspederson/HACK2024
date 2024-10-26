a7ee155648660c409e25c1f9a45265d140cc8f56
Alex Aybar <alexmaybar@gmail.com>
2024-10-25 20:53:27 -0500

    delete models, example state

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import './App.css'
 
 function App() {
+
+
+  const [firstName] = useState("Alex")
 
   return (
     <Tabs
@@ -13,8 +17,8 @@ function App() {
       <Tab eventKey="home" title="Home">
         Tab content for Home
       </Tab>
-      <Tab eventKey="student" title="Student">
-        Tab content for Student File Uploads
+      <Tab eventKey="student" title="Student">  
+        {firstName}
       </Tab>
       <Tab eventKey="validation" title="Validation">
         Tab content for Validation Flow
